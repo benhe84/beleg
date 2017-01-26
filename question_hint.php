a01bcec799f04dfe9af0d00833572a77d9aceed8
benhe84 <benhe84@web.de>
2017-01-26 08:16:28 +0100

    Question hint update

--- question_hint.php
+++ question_hint.php
@@ -5,14 +5,13 @@
     <li>Multiple-Choice Einfachwahlfragen (MC1)</li>
     <li>Multiple-Choice Mehrfachwahlfragenfragen (MC2+)</li>
 </ul>
-<p>Der Hinweis wird beim 2. L&ouml;sungsversuch gezeigt</p>
+<p>Der Hinweis wird beim 2. Versuch gezeigt</p>
 <hr />
 <h4>Freie Fragen</h4>
-<p>In das Feld Antwort 1 ein Regul&auml;rer PHP Ausdruck geschrieben werden. Dieser kann auf der folgenden <a href="https://regex101.com/" target="_blank">Website</a> erzeugt und getestet werden</p>
+<p>In das Feld Antwort 1 wird ein Regul&auml;rer PHP Ausdruck geschrieben. Dieser kann auf der folgenden <a href="https://regex101.com/" target="_blank">Website</a> erzeugt und getestet werden</p>
 <hr />
 <h4>Multiple-Choice Einfachwahlfragen</h4>
-<p>In in das Feld Antwort 1 die Richtige L&ouml;sung einzutrag. Die anderen Felder mit falschen Antworten f&uuml;llen.</p>
+<p>In das Feld Antwort 1 ist die Richtige L&ouml;sung einzutrag. Die anderen Felder mit falschen Antworten f&uuml;llen.</p>
 <hr />
 <h4>Multiple-Choice Mehrfachwahlfragen</h4>
-<p>Je nach Auswahl sind die Antworten 1-Gew&auml;hlt die richtigen. Die anderen Felder mit falschen Antworten f&uuml;llen.<br />
-    Es wird nur Richtig ausgewertet, wenn nur alle richtigen Antworten gew&auml;hlt sind.</p>
+<p>Je nach Auswahl sind die Antworten 1-Gew&auml;hlt die richtigen. Die anderen Felder mit falschen Antworten f&uuml;llen.</p>
