bc4fdcdc7812232170edfce5b2349d192e7ce45c
benhe84 <benhe84@web.de>
2017-01-26 08:14:06 +0100

    Question hint update

--- question_hint.php
+++ question_hint.php
@@ -1,18 +1,18 @@
 <h2>Hinweise zur Fragenerstellung</h2>
 <h3>Es gibt 3 Fragetypen.</h3>
 <ul>
-	<li>Freie Fragen (FF)</li>
-	<li>Multiple-Choice Einfachwahlfragen (MC1)</li>
-	<li>Multiple-Choice Mehrfachwahlfragenfragen (MC2+)</li>
+    <li>Freie Fragen (FF)</li>
+    <li>Multiple-Choice Einfachwahlfragen (MC1)</li>
+    <li>Multiple-Choice Mehrfachwahlfragenfragen (MC2+)</li>
 </ul>
-<p>Bei allen Fragen, wird der eingegebene Hinweis beim 2. L&ouml;sungsversuch gezeigt</p>
+<p>Der Hinweis wird beim 2. L&ouml;sungsversuch gezeigt</p>
 <hr />
 <h4>Freie Fragen</h4>
-<p>Bei freien Fragen genügt es die Antwort in das Feld Antwort 1 einzutragen. Weitere L&ouml;sungen oder verschiedene Schreibweisen k&ouml;nnen in den anderen Feldern plaziert werden</p> 
+<p>In das Feld Antwort 1 ein Regul&auml;rer PHP Ausdruck geschrieben werden. Dieser kann auf der folgenden <a href="https://regex101.com/" target="_blank">Website</a> erzeugt und getestet werden</p>
 <hr />
 <h4>Multiple-Choice Einfachwahlfragen</h4>
 <p>In in das Feld Antwort 1 die Richtige L&ouml;sung einzutrag. Die anderen Felder mit falschen Antworten f&uuml;llen.</p>
 <hr />
 <h4>Multiple-Choice Mehrfachwahlfragen</h4>
-<p>Je nach Auswahl sind die Antworten 1-Gew&auml;hlt die richtigen. Die anderen Felder mit falschen Antworten füllen.<br/>
-Es wird nur Richtig ausgewertet, wenn nur alle richtigen Antworten gew&auml;hlt sind.</p>
+<p>Je nach Auswahl sind die Antworten 1-Gew&auml;hlt die richtigen. Die anderen Felder mit falschen Antworten f&uuml;llen.<br />
+    Es wird nur Richtig ausgewertet, wenn nur alle richtigen Antworten gew&auml;hlt sind.</p>
